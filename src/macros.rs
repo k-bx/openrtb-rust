f372f137f799e44a4aed3d5d878b72371d538c4e
Tim Cheeseman <tcheeseman@appnexus.com>
2018-06-13 10:09:28 -0700

    add copyrights

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2018 The openrtb-rust authors
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 macro_rules! enum_list_module {
     ( $enum_name:ident $variant_type:ty :
       $(
