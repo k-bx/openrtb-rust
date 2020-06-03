ad63a27456a8cce62e8a3df962e908de0f7ac895
Kostiantyn Rybnikov <k-bx@k-bx.com>
2020-06-03 20:30:51 +0300

    Add publisher

---
+++ src/v2_5/publisher.rs
@@ -0,0 +1,24 @@
+// Copyright (c) 2018 The openrtb-rust authors
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use super::category::Category;
+use serde_utils;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Publisher {
+    pub id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub cat: Vec<Category>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub domain: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ext: Option<serde_utils::Ext>,
+}
